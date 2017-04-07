104746fb29fcf5f22312b458e4856d5befac5ea9
Tingwei <tingwei628@users.noreply.github.com>
2017-04-07 23:31:35 +0800

    Update Q1.js

--- Random/Array/Q1.js
+++ Random/Array/Q1.js
@@ -1,8 +1,10 @@
-問題: 一個陣列裡面的物件要怎麼過濾出相同的key value?
-例如: var array = [{a:1, b:1}, {a:1, b:2}, {a:2, b:1}, {a:1, b:1}];
-(同時考慮 a, b的值)
+/*
+ 問題: 一個陣列裡面的物件要怎麼過濾出相同的key value?
+ 例如: var array = [{a:1, b:1}, {a:1, b:2}, {a:2, b:1}, {a:1, b:1}];
+ (同時考慮 a, b的值)
+*/
 
-
+// _ :lodash 符號
 var r = _.filter(_.groupBy(x, (e) => `${e.a}${e.b}`), (k) => {
   if (k.length > 1) {
    return k.map((t) => { t.re = true; return t });
